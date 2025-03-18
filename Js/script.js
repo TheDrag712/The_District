48fdf0d684ec911470a1c8f303da75efd858565e
Guillaume Tom <tomguillaume03@gmail.com>
2025-03-18 16:23:52 +0100

    ajout des fichiers js

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,15 @@
-// // // gtgrdgrdgrd
+// let prenom = prompt("votre prénom?");
+// console.log(prenom);
+// let nom = prompt("Votre nom?");
+// console.log(nom);
+// alert("Bonjour " + prenom + " " + nom);
+// let c = prompt(
+//   prenom + " donnez moi une température en celsius, s'il vous plait"
+// );
+// let f = (c * 9) / 5 + 32;
+// console.log(f);
+// alert(prenom + ", " + c + "°C, en Fahrenheit, cela fait " + f + "°");
+// let num1 = prompt("Donnez le nombre 1")
+// let num2 = prompt("Donnez le nombre 2")
+// let somme = parseInt(num1) + parseInt(num2)
+// console.log(somme)
